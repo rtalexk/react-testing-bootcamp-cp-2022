196a98ac5fa5de301587e82a7acf58b4e7b6f381
Alejandro Rivera <alejandrorivera@Alejandros-MacBook-Pro.local>
2022-04-27 10:46:11 -0600

    refactor: make Footer component better looking

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,25 @@
+import { Box } from "@mui/system";
+import { Toolbar } from "@mui/material";
+import { Typography } from "@mui/material";
 
 export default Footer;
 
 function Footer() {
   return (
-    <footer>
-      Project created during Wizeline Academy React Testing Bootcamp
-    </footer>
+    <Box
+      bottom={0}
+      component="footer"
+      position="fixed"
+      width="100%"
+      borderTop='1px solid #efefef'
+    >
+      <Toolbar >
+        <Box textAlign="center" width="100%">
+          <Typography variant="subtitle1" component="p" fontWeight={300}>
+            Project created during Wizeline Academy React Testing Bootcamp
+          </Typography>
+        </Box>
+      </Toolbar>
+    </Box>
   );
 }
