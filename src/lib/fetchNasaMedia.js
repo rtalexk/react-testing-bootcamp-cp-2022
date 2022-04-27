cf489823fd3b8ed3338ceefdb24e7f9af98390a9
Alejandro Rivera <alejandrorivera@Alejandros-MacBook-Pro.local>
2022-04-27 13:54:09 -0600

    feat: use ENV var for API key

--- src/lib/fetchNasaMedia.js
+++ src/lib/fetchNasaMedia.js
@@ -1,6 +1,7 @@
 
 export default async function fetchNasaMedia(date) {
-  const res = await fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`);
+  const API_KEY = process.env.REACT_APP_NOT_SECRET_CODE;
+  const res = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`);
   const data = await res.json();
   return data;
 }
