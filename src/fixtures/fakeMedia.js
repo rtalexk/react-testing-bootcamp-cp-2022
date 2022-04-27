f0773e8f04697ce9038b0361461cfc47cc6adcaf
Alejandro Rivera <alejandrorivera@Alejandros-MacBook-Pro.local>
2022-04-27 13:35:54 -0600

    fixture: add fakeMedia fixture for testing purposes

---
+++ src/fixtures/fakeMedia.js
@@ -0,0 +1,9 @@
+export default {
+  date: '2022-04-27',
+  explanation: 'What is that large dark spot on Jupiter?',
+  hdurl: 'https://apod.nasa.gov/apod/image/2204/JupiterDarkSpot_JunoTT_3298.jpg',
+  media_type: 'image',
+  service_version: 'v1',
+  title: 'Moon Shadow on Jupiter',
+  url: 'https://apod.nasa.gov/apod/image/2204/JupiterDarkSpot_JunoTT_1080.jpg'
+};
