43a66771cdd28f811c5e073f579e4358d49454a4
Alejandro Rivera <alejandrorivera@Alejandros-MacBook-Pro.local>
2022-04-27 10:15:56 -0600

    fix: remove unnecessary header wrapper in App component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import HomePage from './pages/Home';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <HomePage />
-      </header>
+      <HomePage />
     </div>
   );
 }
