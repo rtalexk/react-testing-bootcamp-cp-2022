b5a17f2a4c8fc14fe41613f2264994a8fcb94deb
Alejandro Rivera <alejandrorivera@Alejandros-MacBook-Pro.local>
2022-04-27 11:33:30 -0600

    feat: add more test cases for ImageDetails component

--- src/components/ImageDetails/ImageDetails.test.js
+++ src/components/ImageDetails/ImageDetails.test.js
@@ -4,15 +4,37 @@ import ImageDetails from './ImageDetails';
 test('Should render image details', () => {
   const title = 'Moon Shadow on Jupiter';
   const timestamp = '2022-04-27';
-  const details = `What is that large dark spot on Jupiter? It's the shadow of Io, one of Jupiter's largest moons. When Jupiter's moons cross between the Jovian giant and the Sun, they created shadows just like when the Earth's moon crosses between the Earth and the Sun. Also like on Earth, if you were in a dark shadow on Jupiter, you would see a moon completely eclipse the Sun. Unlike on Earth, moon shadows occur most days on Jupiter -- what's more unusual is that a spacecraft was close enough to record one with a high-resolution image.  That spacecraft, Juno, was passing so close to Jupiter in late February that nearby clouds and the dark eclipse shadow appear relatively large. Juno has made many discoveries about our Solar System's largest planet, including, recently, rapidly expanding circular auroras.   Explore Your Universe: Random APOD Generator`;
-
-  render(<ImageDetails title={title} timestamp={timestamp} details={details} />);
-
+  const details = `What is that large dark spot on Jupiter?`;
+  
+  render(<ImageDetails title={title} timestamp={timestamp} body={details} />);
+  
   const titleEl = screen.getByRole('heading');
   const timestampEl = screen.getByText(timestamp);
-  const detailsEl = screen.getByRole('paragraph');
-
+  const bodyEl = screen.getByRole('definition');
+  
   expect(titleEl).toBeInTheDocument();
   expect(timestampEl).toBeInTheDocument();
-  expect(detailsEl).toBeInTheDocument();
+  expect(bodyEl).toBeInTheDocument();
+});
+
+test('Do not render image details if title is not provided', () => {
+  const timestamp = '2022-04-27';
+  const details = `What is that large dark spot on Jupiter?`;
+  
+  render(<ImageDetails timestamp={timestamp} body={details} />);
+
+  const detailsEl = screen.queryByRole('article');
+  
+  expect(detailsEl).not.toBeInTheDocument();
+});
+
+test('Do not render image details if body is not provided', () => {
+  const title = 'Moon Shadow on Jupiter';
+  const timestamp = '2022-04-27';
+  
+  render(<ImageDetails title={title} timestamp={timestamp} />);
+  
+  const detailsEl = screen.queryByRole('article');
+  
+  expect(detailsEl).not.toBeInTheDocument();
 });
