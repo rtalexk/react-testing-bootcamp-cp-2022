ed3dc827b3e498b2ae42460481f14f641e2ca927
Alejandro Rivera <alejandrorivera@Alejandros-MacBook-Pro.local>
2022-04-27 13:53:43 -0600

    feat: add ImageCardContainer to HomePage

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import ImageCardContainer from "../../components/ImageCardContainer";
 
 export default HomePage;
 
@@ -7,6 +8,7 @@ function HomePage() {
   return (
     <>
       <Header />
+      <ImageCardContainer />
       <Footer />
     </>
   );
