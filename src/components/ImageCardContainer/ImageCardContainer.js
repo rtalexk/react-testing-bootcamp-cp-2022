8de4ba274eb96966df42dfd65e7779dfaec4d28d
Alejandro Rivera <alejandrorivera@Alejandros-MacBook-Pro.local>
2022-04-27 13:36:36 -0600

    feat: add useNasaMedia to ImageCardContainer

--- src/components/ImageCardContainer/ImageCardContainer.js
+++ src/components/ImageCardContainer/ImageCardContainer.js
@@ -1,9 +1,12 @@
+import useNasaMedia from "../../hooks/useNasaMedia";
 import ImageCard from "../ImageCard/ImageCard";
 
 export default ImageCardContainer;
 
 function ImageCardContainer() {
+  const media = useNasaMedia('2022-04-27');
+
   return (
-    <ImageCard />
+    <ImageCard media={media} />
   );
 }
