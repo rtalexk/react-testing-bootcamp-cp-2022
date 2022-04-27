379cf10c70194d297d9495834886267969070819
Alejandro Rivera <alejandrorivera@Alejandros-MacBook-Pro.local>
2022-04-27 09:51:18 -0600

    refactor: make header better looking

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import Typography from '@mui/material/Typography';
+import { AppBar } from '@mui/material';
+import { Toolbar } from '@mui/material';
+
 import { APP_NAME } from '../../constants';
 
 export default Header;
 
 function Header() {
   return (
-    <header>
-      {APP_NAME}
-
-      <DatePicker
-        label="Pick a day"
-        onChange={() => {}}
-        renderInput={(params) => <TextField {...params} />}
-      />
-    </header>
+    <AppBar color='transparent' position='sticky'>
+      <Toolbar>
+        <Typography variant='h6' sx={{ flexGrow: 1 }}>{APP_NAME}</Typography>
+        <DatePicker
+          label="Pick a day"
+          onChange={() => {}}
+          renderInput={(params) => <TextField variant='standard' {...params} />}
+          maxDate={new Date()}
+        />
+      </Toolbar>
+    </AppBar>
   );
 }
-
-
