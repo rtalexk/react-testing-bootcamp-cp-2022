09faac0abc3128c74405858bb6e07b78ec3baa1d
Alejandro Rivera <alejandrorivera@Alejandros-MacBook-Pro.local>
2022-04-27 10:17:55 -0600

    feat: render Footer in Home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,3 +1,4 @@
+import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 
 export default HomePage;
@@ -6,6 +7,7 @@ function HomePage() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
