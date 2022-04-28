88508a9d4ca6e28d51d31db6ba4465a9066643c3
Alejandro Rivera <alejandrorivera@Alejandros-MacBook-Pro.local>
2022-04-28 14:40:03 -0600

    feat: test error message in ImageCard

--- src/components/ImageCard/ImageCard.test.js
+++ src/components/ImageCard/ImageCard.test.js
@@ -10,13 +10,27 @@ test('Should render Image alongside ImageDetails', () => {
     "service_version": "v1",
     "title": "Moon Shadow on Jupiter",
     "url": "https://apod.nasa.gov/apod/image/2204/JupiterDarkSpot_JunoTT_1080.jpg"
-    };
+  };
 
-    render(<ImageCard media={imageProps} />);
+  render(<ImageCard media={imageProps} />);
 
-    const imageEl = screen.getByRole('img');
-    const titleEl = screen.getByRole('heading');
+  const imageEl = screen.getByRole('img');
+  const titleEl = screen.getByRole('heading');
 
-    expect(imageEl).toBeInTheDocument();
-    expect(titleEl).toBeInTheDocument();
+  expect(imageEl).toBeInTheDocument();
+  expect(titleEl).toBeInTheDocument();
 });
+
+test('Should render an error if error prop is provided', () => {
+  const error = {
+    code: 400,
+    msg: "time data '2022/04/05' does not match format '%Y-%m-%d'",
+    service_version: "v1"
+  }
+
+  render(<ImageCard error={error} />);
+
+  const errorEl = screen.getByRole('heading');
+
+  expect(errorEl).toHaveTextContent(/error/i);
+})
