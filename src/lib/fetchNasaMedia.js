f52ad7c7eeeab5f9bfc269b5b05d7cdcc654cdf1
Alejandro Rivera <alejandrorivera@Alejandros-MacBook-Pro.local>
2022-04-28 14:45:40 -0600

    refactor: clean fetchNasaMedia http request

--- src/lib/fetchNasaMedia.js
+++ src/lib/fetchNasaMedia.js
@@ -1,7 +1,6 @@
 
 export default async function fetchNasaMedia(date) {
   const API_KEY = process.env.REACT_APP_NOT_SECRET_CODE;
-  const res = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`);
-  const data = await res.json();
-  return data;
+  return fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`)
+    .then(res => res.json());
 }
