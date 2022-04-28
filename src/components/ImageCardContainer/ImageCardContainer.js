9c8214ee777f900ca7088d91e72d1047bc4178eb
Alejandro Rivera <alejandrorivera@Alejandros-MacBook-Pro.local>
2022-04-28 10:56:41 -0600

    feat: use current day from context to fetch NASA media

--- src/components/ImageCardContainer/ImageCardContainer.js
+++ src/components/ImageCardContainer/ImageCardContainer.js
@@ -1,10 +1,14 @@
+import { useContext } from "react";
+import { format } from "date-fns";
+import { MediaDateContext } from "../../context/MediaDate";
 import useNasaMedia from "../../hooks/useNasaMedia";
 import ImageCard from "../ImageCard/ImageCard";
 
 export default ImageCardContainer;
 
 function ImageCardContainer() {
-  const media = useNasaMedia('2022-04-27');
+  const { date } = useContext(MediaDateContext);
+  const media = useNasaMedia(format(date, 'yyyy-MM-dd'));
 
   return (
     <ImageCard media={media} />
